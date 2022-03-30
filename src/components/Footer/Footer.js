1f528c1f913c24360cf6a80ccf16c8254d8ea414
Piotr Mól <piotrmolkontakt@gmail.com>
2022-03-30 19:36:10 +0200

    🦶 Footer is now responsive.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,7 @@ import {
   Container,
   Content,
   ContentHeader,
+  ContentSections,
   LinkList,
   Wrapper,
 } from './Footer.styles';
@@ -15,90 +16,94 @@ const Footer = () => {
     <Wrapper>
       <ContentTemplate>
         <Container>
-          <a href='/'>
-            <PiotrMolLogo />
-          </a>
           <Content>
-            <ContentHeader>Pages</ContentHeader>
-            <LinkList>
-              <li>
-                <Link to='/'>Hero</Link>
-              </li>
-              <li>
-                <Link to='/works'>Works</Link>
-              </li>
-              <li>
-                <Link to='/contact'>Contact</Link>
-              </li>
-            </LinkList>
+            <a href='/'>
+              <PiotrMolLogo />
+            </a>
           </Content>
-          <Content>
-            <ContentHeader>Direct Contact</ContentHeader>
-            <LinkList>
-              <li>
-                <a
-                  href='https://t.me/piotrmool'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  Telegram
-                </a>
-              </li>
-              <li>
-                <a
-                  href='https://m.me/pi0trmol'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  Messenger
-                </a>
-              </li>
-              <li>
-                <a href='mailto:piotrmolkontakt@gmail.com'>Mail</a>
-              </li>
-            </LinkList>
-          </Content>
-          <Content>
-            <ContentHeader>Socials</ContentHeader>
-            <LinkList>
-              <li>
-                <a
-                  href='https://www.github.com/s0rus'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  Github
-                </a>
-              </li>
-              <li>
-                <a
-                  href='https://www.linkedin.com/in/piotrmol/'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  LinkedIn
-                </a>
-              </li>
-              <li>
-                <a
-                  href='https://open.spotify.com/playlist/4WUhBPulsN7tXl7dVe4P4w?si=9e0521655c1245db'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  Spotify
-                </a>
-              </li>
-              <li>
-                <a
-                  href='https://www.instagram.com/s0rus_/'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  Instagram
-                </a>
-              </li>
-            </LinkList>
-          </Content>
+          <ContentSections>
+            <Content>
+              <ContentHeader>Pages</ContentHeader>
+              <LinkList>
+                <li>
+                  <Link to='/'>Hero</Link>
+                </li>
+                <li>
+                  <Link to='/works'>Works</Link>
+                </li>
+                <li>
+                  <Link to='/contact'>Contact</Link>
+                </li>
+              </LinkList>
+            </Content>
+            <Content>
+              <ContentHeader>Direct Contact</ContentHeader>
+              <LinkList>
+                <li>
+                  <a
+                    href='https://t.me/piotrmool'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    Telegram
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href='https://m.me/pi0trmol'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    Messenger
+                  </a>
+                </li>
+                <li>
+                  <a href='mailto:piotrmolkontakt@gmail.com'>Mail</a>
+                </li>
+              </LinkList>
+            </Content>
+            <Content>
+              <ContentHeader>Socials</ContentHeader>
+              <LinkList>
+                <li>
+                  <a
+                    href='https://www.github.com/s0rus'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    Github
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href='https://www.linkedin.com/in/piotrmol/'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    LinkedIn
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href='https://open.spotify.com/playlist/4WUhBPulsN7tXl7dVe4P4w?si=9e0521655c1245db'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    Spotify
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href='https://www.instagram.com/s0rus_/'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    Instagram
+                  </a>
+                </li>
+              </LinkList>
+            </Content>
+          </ContentSections>
         </Container>
       </ContentTemplate>
     </Wrapper>
--- src/components/Footer/Footer.styles.js
+++ src/components/Footer/Footer.styles.js
@@ -3,40 +3,50 @@ import GrainTexture from '../../assets/textures/grain-alternate.webp';
 
 export const Wrapper = styled.footer`
   width: 100%;
-
   min-height: 400px;
-
   background: url(${GrainTexture}) ${({ theme: { colors } }) => colors.blue};
   color: ${({ theme: { colors } }) => colors.pink};
 `;
 
 export const Container = styled.div`
   padding: 2rem 0;
-
   display: flex;
   align-items: flex-start;
-
-  @media all and (${({ theme: { screenSizes } }) => screenSizes.mobileLarge}) {
-    flex-flow: column nowrap;
+  @media all and (${({ theme: { screenSizes } }) => screenSizes.small}) {
+    flex-flow: row wrap;
+    justify-content: center;
   }
 `;
 
-export const Content = styled.div`
+export const Content = styled.div``;
+
+export const ContentSections = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-auto-rows: auto;
+  width: 100%;
   margin-left: 2rem;
-  flex: 1;
+
+  @media all and (${({ theme: { screenSizes } }) => screenSizes.small}) {
+    margin-left: 0;
+    margin-top: 2rem;
+    text-align: center;
+  }
+
+  @media all and (${({ theme: { screenSizes } }) => screenSizes.mobileLarge}) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `;
 
 export const ContentHeader = styled.p`
   font-family: ${({ theme: { fonts } }) => fonts.details};
   font-weight: 700;
-
   display: block;
 `;
 
 export const LinkList = styled.ul`
   li {
     margin: 1rem 0;
-
     a {
       transition: letter-spacing 0.2s ease-in-out;
       &:hover,
